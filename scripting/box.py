51787d2c658d237f300ce5c629a6dc9482a47c17
gürkan şahin <33648840+gurkansahin28@users.noreply.github.com>
2024-10-17 00:06:52 +0300

    Update box.py

--- scripting/box.py
+++ scripting/box.py
@@ -1,16 +1,22 @@
-#################################################################
+###################################################################
 import FreeCAD as App
 import FreeCADGui as Gui
+
 docName = "TestingScripting"
 width = 25
 length = 15
 height = 45
 doc = App.newDocument(docName)
 box = doc.addObject("Part::Box", "MyBox")
+
 box.Width = width
 box.Length = length
 box.Height = height
 doc.recompute()
+
+Gui.activateWorkbench("PartWorkbench")
 Gui.SendMsgToActiveView("ViewFit")
 Gui.activeDocument().activeView().viewAxometric()
-###################################################################
+#App.closeDocument(docName)
+
+####################################################################
