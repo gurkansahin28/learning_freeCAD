16ede01342855dd82f2871403bfc8e293506de6f
gürkan şahin <33648840+gurkansahin28@users.noreply.github.com>
2024-10-16 20:34:24 +0300

    Create cylinder.py

---
+++ scripting/cylinder.py
@@ -0,0 +1,11 @@
+import FreeCAD as App
+import FreeCADGui as Gui
+docName = "ScriptingCylinder"
+doc = App.newDocument(docName)
+cylinder = doc.addObject("Part::Cylinder", "myCylinder")
+cylinder.Radius = 5
+cylinder.Height = 15
+doc.recompute()
+
+Gui.SendMsgToActiveView("ViewFit")
+Gui.activeDocument().activeView().viewAxometric()
