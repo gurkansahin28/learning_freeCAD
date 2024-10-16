48f91139ebacd37f8b5b16730b6a8d3bfe6aee3a
gürkan şahin <33648840+gurkansahin28@users.noreply.github.com>
2024-10-16 21:17:57 +0300

    Create catching_from_console.py

---
+++ scripting/catching_from_console.py
@@ -0,0 +1,5 @@
+########################################
+Gui.activateWorkbench("PartWorkbench")
+########################################
+Gui.activateWorkbench("PartDesignWorkbench")
+########################################
