13d93f1215b854ec9ed6cbc360e805603a77b49f
gürkan şahin <33648840+gurkansahin28@users.noreply.github.com>
2024-10-19 12:05:27 +0300

    Update make_box.py

--- scripting/basics/make_box.py
+++ scripting/basics/make_box.py
@@ -1,17 +1,17 @@
 import FreeCAD as App
 import FreeCADGui as Gui
 import Part
-
+###############################################
 docName = "MakingBox"
 doc = App.newDocument(docName)
-
+###
 myPart = doc.addObject("Part::Box", "myBox")
-
+###
 x = y = z = 2
 cube = Part.makeBox(x, y, z)
 myPart.Shape = cube
 doc.recompute()
-
+###
 ### adjustments for the pose of activeView ###################
 Gui.SendMsgToActiveView("ViewFit")
 Gui.activeDocument().activeView().viewAxometric()
