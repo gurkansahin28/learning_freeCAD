009ac911d4cf042cabdd1b7cc9663678df89da99
gürkan şahin <33648840+gurkansahin28@users.noreply.github.com>
2024-10-19 12:12:53 +0300

    Update make_rectangle.py

--- scripting/basics/make_2Ds/make_rectangle.py
+++ scripting/basics/make_2Ds/make_rectangle.py
@@ -1,7 +1,7 @@
 import FreeCAD as App
 import FreeCADGui as Gui
 import Draft
-import Part
+
 ################################################
 docName = "MakeRectangle"
 doc = App.newDocument(apName)
@@ -15,3 +15,5 @@ doc.recompute()
 Gui.SendMsgToActiveView("ViewFit")
 Gui.activeDocument().activeView().viewAxometric()
 Gui.ActiveDocument.ActiveView.setAxisCross(True)
+#doc.saveAs("/absolute/path/to/your/file.FCStd")
+# App.closeDocument(docName)
