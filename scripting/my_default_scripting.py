ac9d1d30e850a53acbe9b201f1db5108ce029c3f
gürkan şahin <33648840+gurkansahin28@users.noreply.github.com>
2024-10-19 11:59:04 +0300

    Update my_default_scripting.py

--- scripting/my_default_scripting.py
+++ scripting/my_default_scripting.py
@@ -1,3 +1,15 @@
 import FreeCAD as App
 import FreeCADGui as Gui
+import Draft
+import Part
+#################################################
+docName = ""
+doc = App.newDocument(docName)
 
+
+
+##################################################
+Gui.SendMsgToActiveView("ViewFit")
+Gui.activeDocument().activeView().viewAxometric()
+Gui.ActiveDocument.ActiveView.setAxisCross(True)
+###################################################
