6ff4eae0f4b10d888f3d486fbf6569957b950ac5
gürkan şahin <33648840+gurkansahin28@users.noreply.github.com>
2024-10-30 16:49:43 +0300

    Update Draft_phrases.py

--- scripting/Draft_phrases.py
+++ scripting/Draft_phrases.py
@@ -9,11 +9,26 @@ myRect = Draft.makeRectangle(4, 3)
 myDistance = App.Vector(2, 2, 0)
 Draft.move(myRect, myDistance)
 
+Draft.move(App.Obj or List, Vector, [copymode])
+
+Draft.rotate(App.Obj or List, angle, [center], [axis], [copymode])
+
+Draft.scale(App.Obj or List, Vector, [center], [copymode])
+
 myCutObj = Draft.cut(ObjOne, ObjTwo)
 
 extrude = Draft.extrude(Obj, Vector)
 
 fuse = Draft.fuse(ObjectOne, ObjectTwo)
 
+Draft.makeLine(Vector, Vector)
+
+Draft.makeWire(list of Part.Wire, [closed], [placement], [facemode])
+
+Draft.makeCircle(radius, [placement], [facemode], [startangle], [endangle])
+
+Draft.select(App.Obj)
+
+Draft.getSelection()
 
 
