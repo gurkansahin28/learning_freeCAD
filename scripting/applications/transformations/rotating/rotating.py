f8335a74b36444d8d32273944920c87d71b1cc3d
gürkan şahin <33648840+gurkansahin28@users.noreply.github.com>
2024-10-31 20:57:16 +0300

    code improvement

--- scripting/applications/transformations/rotating/rotating.py
+++ scripting/applications/transformations/rotating/rotating.py
@@ -1,17 +1,39 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 31 20:49:44 2024
+
+@author: gurkan
+"""
+# aliases for convenient coding
 import FreeCAD as App
 import FreeCADGui as Gui
 
-docName = "RotateTheObject"
+# creating a new document given a name
+docName = "RotatingObjectDoc"
 doc = App.newDocument(docName)
-box = doc.addObject("Part::Box", "MyBox")
-
-box.Length = 10
-box.Width = 5
-box.Height = 25
-
-box.Placement.Rotation = App.Rotation(App.Vector(0, 0, 1), 45)
 doc.recompute()
 
+# creating a cube instance
+width = length = height = 10
+cube = doc.addObject('Part::Box', 'myCube')
+
+# giving values required 
+cube.Length = length
+cube.Width = width
+cube.Height = height
+
+# setting up the placement of the cube
+cube.Placement.Base = App.Vector(5, 5, 0)
+
+# rotating the cube 20 degree around the Z axis
+cube.Placement.Rotation = App.Rotation(App.Vector(0, 0, 1), 20)
+doc.recompute()
+
+##################################################
 Gui.activeDocument().activeView().viewAxometric()
-Gui.SendMsgToActiveView("ViewFit")
+Gui.ActiveDocument.ActiveView.setAxisCross(True)
+Gui.SendMsgToActiveView('ViewFit')
+###################################################
+
 #App.closeDocument(docName)
