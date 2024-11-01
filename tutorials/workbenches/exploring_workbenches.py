fd97d53d283635f27b0ae6e4056277efaf0bb444
gürkan şahin <33648840+gurkansahin28@users.noreply.github.com>
2024-11-01 17:08:57 +0300

    adding a new directory named workbenches to put some code documents about exploring the workbenches in FreeCAD scripting

---
+++ tutorials/workbenches/exploring_workbenches.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 24 14:34:37 2024
+
+@author: gurkan
+"""
+import FreeCAD as App
+import FreeCADGui as Gui
+#------------------------------------------------------------
+Gui.runCommand('Std_Workbench',1)
+# Gui.activateWorkbench("DraftWorkbench")
+
+Gui.runCommand('Std_Workbench', 2)
+# Gui.activateWorkbench("FemWorkbench")
+
+Gui.runCommand('Std_Workbench', 3)
+# Gui.activateWorkbench("InspectionWorkbench")
+
+Gui.runCommand('Std_Workbench', 4)
+# Gui.activateWorkbench("MeshWorkbench")
+
+Gui.runCommand('Std_Workbench', 5)
+# Gui.activateWorkbench("OpenSCADWorkbench")
+
+Gui.runCommand('Std_Workbench', 6)
+# Gui.activateWorkbench("PartDesignWorkbench")
+
+Gui.runCommand('Std_Workbench',7)
+# Gui.activateWorkbench("PartWorkbench")
+
+Gui.runCommand('Std_Workbench', 8)
+# Gui.activateWorkbench("PathWorkbench")
+
+Gui.runCommand('Std_Workbench', 9)
+# Gui.activateWorkbench("PointsWorkbench")
+
+Gui.runCommand('Std_Workbench', 10)
+# Gui.activateWorkbench("ReverseEngineeringWorkbench")
+
+Gui.runCommand('Std_Workbench', 11)
+# Gui.activateWorkbench("RobotWorkbench")
+
+Gui.runCommand('Std_Workbench',12)
+# Gui.activateWorkbench("SketcherWorkbench")
+
+Gui.runCommand('Std_Workbench', 13)
+# Gui.activateWorkbench("SpreadsheetWorkbench")
+
+Gui.runCommand('Std_Workbench',14)
+# Gui.activateWorkbench("StartWorkbench")
+
+Gui.runCommand('Std_Workbench', 15)
+# Gui.activateWorkbench("SurfaceWorkbench")
+
+Gui.runCommand('Std_Workbench', 16)
+# Gui.activateWorkbench("TechDrawWorkbench")
+
+Gui.runCommand('Std_Workbench', 17)
+# Gui.activateWorkbench("WebWorkbench")
+
+Gui.runCommand('Std_Workbench', 18)
+### FreeCAD quited abruptly.....
