c052b27c342cdc062a0777f43e0657c33896d180
gürkan şahin <33648840+gurkansahin28@users.noreply.github.com>
2024-11-04 21:38:04 +0300

    refinments

--- scripting/phrases/dummy_console.py
+++ scripting/phrases/dummy_console.py
@@ -9,7 +9,7 @@ doc = App.newDocument(docName)
 
 App.setActiveDocument("CreationCones")
 App.ActiveDocument=App.getDocument("CreationCones")
-Gui.ActiveDocument=Gui.getDocument("CreationConees")
+Gui.ActiveDocument=Gui.getDocument("CreationCones")
 
 r_bottom = 0
 r_top = 5
@@ -25,6 +25,14 @@ coneOther.Radius2 = r_top
 coneOther.Height = height
 coneOther.Placement.Base = App.Vector(3, 3, 0)
 doc.recompute()
+
+Gui.activeDocument().activeView().viewTrimetric()
+Gui.SendMsgToActiveView("ViewFit")
+
+from BOPTools import BOPFeatures
+bp = BOPFeatures.BOPFeatures(App.activeDocument())
+bp.make_common(["MyCone", "MyOtherCone", ])
+
 
 # App.setActiveDocument("")
 # App.ActiveDocument=None
