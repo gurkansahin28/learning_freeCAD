da4b09226493da39da36923f63ba32bec8da0154
gürkan şahin <33648840+gurkansahin28@users.noreply.github.com>
2024-11-04 21:29:40 +0300

    coding alternatively

--- scripting/phrases/dummy_console.py
+++ scripting/phrases/dummy_console.py
@@ -10,3 +10,18 @@ doc = App.newDocument(docName)
 App.setActiveDocument("CreationBoxes")
 App.ActiveDocument=App.getDocument("CreationBoxes")
 Gui.ActiveDocument=Gui.getDocument("CreationBoxes")
+
+r_bottom = 0
+r_top = 5
+height = 10
+cone = Part.makeCone(r_bottom, r_top, height)
+coneObj = doc.addObject('Part::Feature', 'MyCone')
+coneObj.Shape = cone
+doc.recompute()
+
+coneOther = doc.addObject('Part::Cone', 'MyOtherCone')
+coneOther.Radius1 = r_bottom
+coneOther.Radius2 = r_top
+coneOther.Height = height
+coneOther.Placement.Base = App.Vector(3, 3, 0)
+doc.recompute()
