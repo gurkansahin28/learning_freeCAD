c6f68128c628cb11d98f66105566aab218c29eac
gürkan şahin <33648840+gurkansahin28@users.noreply.github.com>
2024-11-06 21:40:04 +0300

    Update exploring_workbenches.py
    Code refinements

--- tutorials/workbenches/exploring_workbenches.py
+++ tutorials/workbenches/exploring_workbenches.py
@@ -10,54 +10,71 @@ import FreeCADGui as Gui
 #------------------------------------------------------------
 Gui.runCommand('Std_Workbench',1)
 # Gui.activateWorkbench("DraftWorkbench")
+input('Please press the Enter key!')
 
 Gui.runCommand('Std_Workbench', 2)
 # Gui.activateWorkbench("FemWorkbench")
+input('Please press the Enter key!')
 
 Gui.runCommand('Std_Workbench', 3)
 # Gui.activateWorkbench("InspectionWorkbench")
+input('Please press the Enter key!')
 
 Gui.runCommand('Std_Workbench', 4)
 # Gui.activateWorkbench("MeshWorkbench")
+input('Please press the Enter key!')
 
 Gui.runCommand('Std_Workbench', 5)
 # Gui.activateWorkbench("OpenSCADWorkbench")
+input('Please press the Enter key!')
 
 Gui.runCommand('Std_Workbench', 6)
 # Gui.activateWorkbench("PartDesignWorkbench")
+input('Please press the Enter key!')
 
 Gui.runCommand('Std_Workbench',7)
 # Gui.activateWorkbench("PartWorkbench")
+input('Please press the Enter key!')
 
 Gui.runCommand('Std_Workbench', 8)
 # Gui.activateWorkbench("PathWorkbench")
+input('Please press the Enter key!')
 
 Gui.runCommand('Std_Workbench', 9)
 # Gui.activateWorkbench("PointsWorkbench")
+input('Please press the Enter key!')
 
 Gui.runCommand('Std_Workbench', 10)
 # Gui.activateWorkbench("ReverseEngineeringWorkbench")
+input('Please press the Enter key!')
 
 Gui.runCommand('Std_Workbench', 11)
 # Gui.activateWorkbench("RobotWorkbench")
+input('Please press the Enter key!')
 
 Gui.runCommand('Std_Workbench',12)
 # Gui.activateWorkbench("SketcherWorkbench")
+input('Please press the Enter key!')
 
 Gui.runCommand('Std_Workbench', 13)
 # Gui.activateWorkbench("SpreadsheetWorkbench")
+input('Please press the Enter key!')
 
 Gui.runCommand('Std_Workbench',14)
 # Gui.activateWorkbench("StartWorkbench")
+input('Please press the Enter key!')
 
 Gui.runCommand('Std_Workbench', 15)
 # Gui.activateWorkbench("SurfaceWorkbench")
+input('Please press the Enter key!')
 
 Gui.runCommand('Std_Workbench', 16)
 # Gui.activateWorkbench("TechDrawWorkbench")
+input('Please press the Enter key!')
 
 Gui.runCommand('Std_Workbench', 17)
 # Gui.activateWorkbench("WebWorkbench")
+input('Please press the Enter key!')
 
 Gui.runCommand('Std_Workbench', 18)
 ### FreeCAD quited abruptly.....
