931cff48bc3177bcab42fb8d7e32581ea34665a6
gürkan şahin <33648840+gurkansahin28@users.noreply.github.com>
2024-11-08 23:23:55 +0300

    Update vector_arithmetic.py

--- tutorials/placement/vector_arithmetic.py
+++ tutorials/placement/vector_arithmetic.py
@@ -27,13 +27,16 @@ myBox.Width = width
 myBox.Length = length
 myBox.Height = height
 doc.recompute()
+
 input('Press enter to continue!')
+
 # 4. setting up the visualization
 print('setting the visual presentation...')
 Gui.ActiveDocument.ActiveView.setAxisCross(True)
 Gui.activeDocument().activeView().viewTrimetric()
 Gui.runCommand('Draft_ToggleGrid',1)
 Gui.SendMsgToActiveView("ViewFit")
+
 input('Press enter to continue!')
 
 # 5. getting the existing object placement values
@@ -43,6 +46,7 @@ print(startingPlaBase.x)
 print(startingPlaBase.y)
 print(startingPlaBase.z)
 doc.recompute()
+
 input('Press enter to continue!')
 
 print('setting parameters one by one...')
@@ -57,6 +61,7 @@ print('changing base of placement by adding...')
 newPlaBase = App.Vector(3, 0, 0)
 myBox.Placement.Base += newPlaBase
 doc.recompute()
+
 input('Press enter to continue!')
 
 newerPlaBase = App.Vector(0, 3, 0)
@@ -64,6 +69,7 @@ resultVector = newPlaBase + newerPlaBase
 print(resultVector)
 myBox.Placement.Base = resultVector
 doc.recompute()
+
 input('Press enter to continue!')
 
 # 6.2. Subtraction
@@ -74,6 +80,7 @@ newPlaBase = App.Vector(3, 3, 0)
 resultVector = existingPlaBase - newPlaBase
 myBox.Placement.Base = resultVector
 doc.recompute()
+
 input('Press enter to continue!')
 
 # 6.3. Scaling
@@ -84,6 +91,4 @@ scalerVector = existingPlaBase * -2
 print(scalerVector)
 myBox.Placement.Base = scalerVector
 doc.recompute()
-input('Press enter to continue!')
-
 # note: there are advanced topics like DOT and CROSS products
