ddee612eb138b57b83894aba028b72d37fb1f314
gürkan şahin <33648840+gurkansahin28@users.noreply.github.com>
2024-11-24 16:13:15 +0300

    Update chamfer.py
    reviewing the code

--- tutorials/chamfer.py
+++ tutorials/chamfer.py
@@ -10,31 +10,34 @@ import Part
 import FreeCAD as App
 import FreeCADGui as Gui
 
-# creating a document via a name and label
+# Creating a document via a name and label
 docName = 'ChamferDoc'
 doc = App.newDocument(docName)
 doc.Label = docName
 
-# creating a box from the Part library
+# Creating a box from the Part library
 objFromLib = 'Part::Box'
 objName = 'Box'
 box = App.ActiveDocument.addObject(objFromLib, objName)
 doc.recompute()
 
 ### Chamfer Operations
-## declearing doc, baseObj, fillets, library and object name
+## declaring doc, baseObj, fillets, library and object name
 doc = App.getDocument(docName)
 baseObj = doc.getObject('Box')
 __fillets__ = []
 objFromLib = 'Part::Chamfer'
 objName = 'Chamfer'
-# adding chamfer object to the document
+size1 = 1.00
+size2 = 1.00
+# Adding chamfer object to the document
 chamfer = doc.addObject(objFromLib, objName)
 chamfer.Base = baseObj
-# creating and stroring chamfer values for the edges
+# Creating and storing chamfer values for the edges
 for edge in range(1, 13):
-    __fillets__.append((edge, 1.00, 1.00))
-# applying the edge related chamfer values
+    __fillets__.append((edge, size1, size2))
+
+# applying the edge-related chamfer values
 chamfer.Edges = __fillets__
 del __fillets__
 baseObj.Visibility = False
